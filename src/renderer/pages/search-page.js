e0c45866e43e8a3ff9c451e76432c1f9f0f800cc
Niv Govrin <niv.govrin@gmail.com>
2017-12-07 16:52:37 +0200

    nearly finished torrent search. now adding to list works + design

--- src/renderer/pages/search-page.js
+++ src/renderer/pages/search-page.js
@@ -1,4 +1,5 @@
 const React = require('react')
+// const colors = require('material-ui/styles/colors')
 // const prettyBytes = require('prettier-bytes')
 
 // const Checkbox = require('material-ui/Checkbox').default
@@ -43,46 +44,24 @@ module.exports = class SearchPage extends React.Component {
 
   renderResult (searchResult, index) {
     // const state = this.props.state
-    // const infoHash = torrentSummary.infoHash
-    // const isSelected = infoHash && state.selectedInfoHash === infoHash
-
-    // Background image: show some nice visuals, like a frame from the movie, if possible
-    // const style = {}
-    // if (torrentSummary.posterFileName) {
-    //   const gradient = 'linear-gradient(to bottom, rgba(0, 0, 0, 0.4) 0%, rgba(0, 0, 0, 0.4) 100%)'
-    //   const posterPath = TorrentSummary.getPosterPath(torrentSummary)
-    //   style.backgroundImage = `${gradient}, url('${posterPath}')`
-    // }
-
-    // Foreground: name of the torrent, basic info like size, play button,
-    // cast buttons if available, and delete
     const classes = ['search-result']
-    // if (isSelected) classes.push('selected')
-    // if (!infoHash) classes.push('disabled')
-    // if (!torrentSummary.torrentKey) throw new Error('Missing torrentKey')
     return (
       <div
-        // id={torrentSummary.testID && ('torrent-' + torrentSummary.testID)}
-        // key={torrentSummary.torrentKey}
-        // style={style}
         onClick={(e)=>this.handleClick(searchResult)}
         className={classes.join(' ')}
-        // onContextMenu={infoHash && dispatcher('openTorrentContextMenu', infoHash)}
-        /*onClick={infoHash && dispatcher('toggleSelectTorrent', infoHash)}*/>
-        {/* {this.renderTorrentMetadata(torrentSummary)} */}
-        {/* {infoHash ? this.renderTorrentButtons(torrentSummary) : null} */}
-        {/* {isSelected ? this.renderTorrentDetails(torrentSummary) : null} */}
-        <div key='title' className='title'>{searchResult.title}</div>
-        <div key='seeds' className='seeds'>
-          Seeds: {searchResult.seeds}
-        </div>
+        >
+        <h3 key='title' className='title'>
+          {searchResult.title}
+        </h3>
         <div key='peers' className='peers'>
           Peers: {searchResult.peers}
+        </div>
+        <div key='seeds' className='seeds'>
+          Seeds: {searchResult.seeds}
         </div>
         <div key='size' className='size'>
           Size: {searchResult.size}
         </div>
-        <hr />
       </div>
     )
   }
